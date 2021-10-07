6ec45c76a3e2344c2efcbd5ff347ad29e002194e
Vadim <vadim.mukhmd@gmail.com>
2021-10-07 22:57:26 +0500

    Create index.php
    Главная страница

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мухамедьяров В.А.</title>
+</head>
+<body>
+    <h1>Лабораторные работы</h1>
+    <h2>студента гр. ПИ-320 Мухамедьярова В.А.</h2>
+    
+    <ul>
+        <li><a href = "WWW/Muhamedyarov/LR1/lab1.html">Лабораторная работа №1</a></li>
+        <li><a href = "WWW/Muhamedyarov/LR2/Lab2.html">Лабораторная работа №2</a></li>
+        <li><a href = "WWW/Muhamedyarov/LR3/lab3.html">Лабораторная работа №3</a></li>
+        <li><a href = "WWW/Muhamedyarov/LR4/lab4_1.html">Лабораторная работа №4</a></li>
+        <li><a href = "WWW/Muhamedyarov/LR5/lab5.html">Лабораторная работа №5</a></li>
+    </ul>
+</body>
+</html>
